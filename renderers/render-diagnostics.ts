b4ef1c00291429c2f9110655dfca406dafd32c43
Jim Kang <jimkang@gmail.com>
2023-03-08 11:43:32 -0500

    Add (somewhat) distinguishing colors for physics bodies.

--- renderers/render-diagnostics.ts
+++ renderers/render-diagnostics.ts
@@ -12,13 +12,19 @@ export function renderPhysicsDiagnostics({ souls }) {
 }
 
 function renderBounds({ bodies }) {
-  var edges = bodies.map((body) => verticesToEdges(body.vertices)).flat();
+  var edges = bodies
+    .map((body) => verticesToEdges(body.id, body.vertices))
+    .flat();
+  console.log(
+    'body ids for edges',
+    edges.map((edge) => edge.bodyId)
+  );
   var lines = diagnosticsRoot.selectAll('line').data(edges, (edge) => edge.id);
   lines.exit().remove();
   lines
     .enter()
     .append('line')
-    .attr('stroke', 'hsl(120, 50%, 50%)')
+    .attr('stroke', getBodyColor)
     .merge(lines)
     .attr('x1', (edge) => edge.x1)
     .attr('y1', (edge) => edge.y1)
@@ -33,16 +39,16 @@ function renderBodyCenters({ bodies }) {
     .enter()
     .append('circle')
     .attr('r', 10)
-    .attr('fill', 'hsl(150, 80%, 50%)')
+    .attr('fill', getBodyColor)
     .merge(dots)
     .attr('cx', (body) => body.position.x)
     .attr('cy', (body) => body.position.y);
 }
 
-function verticesToEdges(vertices) {
+function verticesToEdges(bodyId, vertices) {
   var edges = [];
   for (let i = 0; i < vertices.length - 1; ++i) {
-    edges.push(vertexPairToEdge(vertices[i], vertices[i + 1]));
+    edges.push(vertexPairToEdge(bodyId, vertices[i], vertices[i + 1]));
   }
   if (edges.length > 2) {
     let lastEdge = edges[edges.length - 1];
@@ -53,17 +59,23 @@ function verticesToEdges(vertices) {
       x2: firstEdge.x1,
       y2: firstEdge.y1,
       id: [lastEdge.x2, lastEdge.y2, firstEdge.x1, firstEdge.y1].join('_'),
+      bodyId,
     });
     return edges;
   }
 }
 
-function vertexPairToEdge(v1, v2) {
+function vertexPairToEdge(bodyId, v1, v2) {
   return {
     x1: v1.x,
     y1: v1.y,
     x2: v2.x,
     y2: v2.y,
     id: [v1.x, v1.y, v2.x, v2.y].join('_'),
+    bodyId,
   };
+}
+
+function getBodyColor(obj) {
+  return `hsl(${((obj.bodyId || obj.id) * 47) % 360}, 80%, 50%)`;
 }
